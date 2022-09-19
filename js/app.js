b1c9497c7dd7c5033f320cb9f69370fc1a54dbb8
castillomiguel25 <miguelangelcastillo947@gmail.com>
2022-09-19 15:59:03 -0400

    portafolio

--- js/app.js
+++ js/app.js
@@ -1,224 +1,49 @@
-// header
-window.addEventListener("scroll", function () {
+// portafolioooooo
 
-  let header = document.getElementById('header1')
-  let position = header.getBoundingClientRect().top
-  console.log(position);
-  let pantalla = window.innerHeight / 3.5;
-
-
-  if (position < pantalla) {
-    header.style.background = 'rgba(32,35,41,.8)'
-    header.style.height = '100px'
-  }
-
-});
-
-
-
-// lenguajess
-
-$(".dropdown img.flag").addClass("flagvisibility");
-$(".dropdown dt a").click(function () {
-  $(".dropdown dd ul").toggle();
-});
-
-$(".dropdown dd ul li a").click(function () {
-  var text = $(this).html();
-  $(".dropdown dt a span").html(text);
-  $(".dropdown dd ul").hide();
-  $("#result").html("Selected value is: " + getSelectedValue("sample"));
-});
-
-function getSelectedValue(id) {
-  return $("#" + id).find("dt a span.value").html();
+function mostrar () {
+    document.getElementById('project').style.display = 'block';
 }
 
-$(document).bind('click', function (e) {
-  var $clicked = $(e.target);
-  if (!$clicked.parents().hasClass("dropdown"))
-    $(".dropdown dd ul").hide();
-});
+function ocultar () {
+    document.getElementById('project').style.display = 'none';
+}
 
-$(".dropdown img.flag").toggleClass("flagvisibility");
+function mostrar2 () {
+    document.getElementById('project2').style.display = 'block';
+}
 
+function ocultar2 () {
+    document.getElementById('project2').style.display = 'none';
+}
 
-// colorr
+function mostrar3 () {
+    document.getElementById('project3').style.display = 'block';
+}
 
-window.addEventListener("scroll", function () {
+function ocultar3 () {
+    document.getElementById('project3').style.display = 'none';
+}
 
-  let anima = document.getElementById('animations')
-  let position = anima.getBoundingClientRect().top
-  console.log(position);
-  let pantalla = window.innerHeight / 3.5;
+function mostrar4 () {
+    document.getElementById('project4').style.display = 'block';
+}
 
-  setTimeout(function () {
-    if (position < pantalla) {
-      anima.style.color = 'rgba(32,35,41,.8)'
-    } else {
-      {
-        anima.style.color = 'white'
-      }
-    }
-  }, 500)
+function ocultar4 () {
+    document.getElementById('project4').style.display = 'none';
+}
 
-});
+function mostrar5 () {
+    document.getElementById('project5').style.display = 'block';
+}
 
-window.addEventListener("scroll", function () {
+function ocultar5 () {
+    document.getElementById('project5').style.display = 'none';
+}
 
-  let anima = document.getElementById('anima')
-  let position = anima.getBoundingClientRect().top
-  console.log(position);
-  let pantalla = window.innerHeight / 3.5;
+function mostrar6 () {
+    document.getElementById('project6').style.display = 'block';
+}
 
-  setTimeout(function () {
-    if (position < pantalla) {
-      anima.style.color = 'rgba(32,35,41,.8)'
-    } else {
-      {
-        anima.style.color = 'white'
-      }
-    }
-  }, 500)
-
-});
-
-
-window.addEventListener("scroll", function () {
-
-  let anima = document.getElementById('anim')
-  let position = anima.getBoundingClientRect().top
-  console.log(position);
-  let pantalla = window.innerHeight / 3.5;
-
-  setTimeout(function () {
-    if (position < pantalla) {
-      anima.style.color = 'rgba(32,35,41,.8)'
-    } else {
-      {
-        anima.style.color = 'white'
-      }
-    }
-  }, 500)
-});
-
-window.addEventListener("scroll", function () {
-
-  let anima = document.getElementById('anim')
-  let position = anima.getBoundingClientRect().top
-  console.log(position);
-  let pantalla = window.innerHeight / 3.5;
-
-  setTimeout(function () {
-    if (position < pantalla) {
-      anima.style.color = 'rgba(32,35,41,.8)'
-    } else {
-      {
-        anima.style.color = 'white'
-      }
-    }
-  }, 500)
-});
-
-window.addEventListener("scroll", function () {
-
-  let anima = document.getElementById('ani')
-  let position = anima.getBoundingClientRect().top
-  console.log(position);
-  let pantalla = window.innerHeight / 3.5;
-
-  setTimeout(function () {
-    if (position < pantalla) {
-      anima.style.color = 'rgba(32,35,41,.8)'
-    } else {
-      {
-        anima.style.color = 'white'
-      }
-    }
-  }, 500)
-});
-
-window.addEventListener("scroll", function () {
-
-  let anima = document.getElementById('an')
-  let position = anima.getBoundingClientRect().top
-  console.log(position);
-  let pantalla = window.innerHeight / 3.5;
-  if (position < pantalla) {
-    anima.style.color = 'rgba(32,35,41,.8)'
-  } else {
-    {
-      anima.style.color = 'white'
-    }
-  }
-});
-
-window.addEventListener("scroll", function () {
-
-  let anima = document.getElementById('a')
-  let position = anima.getBoundingClientRect().top
-  console.log(position);
-  let pantalla = window.innerHeight / 3.5;
-  if (position < pantalla) {
-    anima.style.color = 'rgba(32,35,41,.8)'
-  } else {
-    {
-      anima.style.color = 'white'
-    }
-  }
-});
-
-window.addEventListener("scroll", function () {
-
-  let anima = document.getElementById('1')
-  let position = anima.getBoundingClientRect().top
-  console.log(position);
-  let pantalla = window.innerHeight / 3.5;
-  if (position < pantalla) {
-    anima.style.color = 'rgba(32,35,41,.8)'
-  } else {
-    {
-      anima.style.color = 'white'
-    }
-  }
-});
-
-window.addEventListener("scroll", function () {
-
-  let anima = document.getElementById('2')
-  let position = anima.getBoundingClientRect().top
-  console.log(position);
-  let pantalla = window.innerHeight / 3.5;
-  if (position < pantalla) {
-    anima.style.color = 'rgba(32,35,41,.8)'
-  } else {
-    {
-      anima.style.color = 'white'
-    }
-  }
-});
-
-window.addEventListener("scroll", function () {
-
-  let anima = document.getElementById('3')
-  let position = anima.getBoundingClientRect().top
-  console.log(position);
-  let pantalla = window.innerHeight / 3.5;
-  if (position < pantalla) {
-    anima.style.color = 'rgba(32,35,41,.8)'
-  } else {
-    {
-      anima.style.color = 'white'
-    }
-  }
-});
-
-
-// function mostrar () {
-//   document.getElementById('caja').style.opacity = '1';
-// }
-// function ocultar () {
-//   document.getElementById('caja').style.opacity = '0';
-// }
-
-
+function ocultar6 () {
+    document.getElementById('project6').style.display = 'none';
+}
