d8845f55d32370f91568afe1e4805011d5ffc880
castillomiguel25 <miguelangelcastillo947@gmail.com>
2022-09-19 22:51:36 -0400

    finish

--- js/app.js
+++ js/app.js
@@ -1,49 +1,174 @@
 // portafolioooooo
 
-function mostrar () {
+function mostrar() {
     document.getElementById('project').style.display = 'block';
 }
 
-function ocultar () {
+function ocultar() {
     document.getElementById('project').style.display = 'none';
 }
 
-function mostrar2 () {
+function mostrar2() {
     document.getElementById('project2').style.display = 'block';
 }
 
-function ocultar2 () {
+function ocultar2() {
     document.getElementById('project2').style.display = 'none';
 }
 
-function mostrar3 () {
+function mostrar3() {
     document.getElementById('project3').style.display = 'block';
 }
 
-function ocultar3 () {
+function ocultar3() {
     document.getElementById('project3').style.display = 'none';
 }
 
-function mostrar4 () {
+function mostrar4() {
     document.getElementById('project4').style.display = 'block';
 }
 
-function ocultar4 () {
+function ocultar4() {
     document.getElementById('project4').style.display = 'none';
 }
 
-function mostrar5 () {
+function mostrar5() {
     document.getElementById('project5').style.display = 'block';
 }
 
-function ocultar5 () {
+function ocultar5() {
     document.getElementById('project5').style.display = 'none';
 }
 
-function mostrar6 () {
+function mostrar6() {
     document.getElementById('project6').style.display = 'block';
 }
 
-function ocultar6 () {
+function ocultar6() {
     document.getElementById('project6').style.display = 'none';
 }
+
+
+// blogg
+
+function mostrare() {
+    const elements = ['html', 'htmls', 'css', 'csss', 'js', 'vues', 'reacts', 'gits', 'javascript', 'vue', 'react', 'git', 'htmll', 'cssl', 'jsl', 'vuel', 'reactl', 'gitl'].
+        map(id => document.getElementById(id).style.display = 'block')
+    console.log(elements)
+}
+
+
+function o() {
+    document.getElementById('html').style.display = 'none';
+}
+
+
+
+function o2() {
+    document.getElementById('css').style.display = 'none';
+}
+
+
+
+function o3() {
+    document.getElementById('javascript').style.display = 'none';
+}
+
+
+
+function o4() {
+    document.getElementById('vue').style.display = 'none';
+}
+
+
+
+function o5() {
+    document.getElementById('react').style.display = 'none';
+}
+
+
+function o6() {
+    document.getElementById('git').style.display = 'none';
+}
+
+
+
+
+
+
+
+
+
+
+
+function oh() {
+    document.getElementById('htmls').style.display = 'none';
+}
+
+
+
+function oh2() {
+    document.getElementById('csss').style.display = 'none';
+}
+
+
+
+function oh3() {
+    document.getElementById('js').style.display = 'none';
+}
+
+
+
+function oh4() {
+    document.getElementById('vues').style.display = 'none';
+}
+
+
+
+function oh5() {
+    document.getElementById('reacts').style.display = 'none';
+}
+
+
+function oh6() {
+    document.getElementById('gits').style.display = 'none';
+}
+
+
+
+
+
+
+function ohs() {
+    document.getElementById('htmll').style.display = 'none';
+}
+
+
+
+function oh2s() {
+    document.getElementById('cssl').style.display = 'none';
+}
+
+
+
+function oh3s() {
+    document.getElementById('jsl').style.display = 'none';
+}
+
+
+
+function oh4s() {
+    document.getElementById('vuel').style.display = 'none';
+}
+
+
+
+function oh5s() {
+    document.getElementById('reactl').style.display = 'none';
+}
+
+
+function oh6s() {
+    document.getElementById('gitl').style.display = 'none';
+}
+
